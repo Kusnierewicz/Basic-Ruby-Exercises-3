0909dad7add6e78a2cc19399fc19e0bb2d294d32
Kusnierewicz <kusnierewicz@gmail.com>
2014-11-10 21:43:49 +0100

    palindrome code added

--- pal.rb
+++ pal.rb
@@ -1,3 +1,18 @@
 def pal(n)
-  n
+  if n.length == 0
+  	true
+  elsif n.length == 1
+  	true
+  elsif n[0] == n[-1]
+  	pal(n[1..-2])
+  elsif n[0] != n[-1]
+  	false
+  end
 end
+
+puts pal("a")
+puts pal("aa")
+puts pal("ab")
+puts pal("aba")
+puts pal("abba")
+
