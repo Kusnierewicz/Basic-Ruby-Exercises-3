f888cb8574c5f13b92a13470c69aae26719e2db7
Kusnierewicz <kusnierewicz@gmail.com>
2014-11-11 15:09:45 +0100

    merge_sort in production

---
+++ merge_sort.rb
@@ -0,0 +1,28 @@
+
+def merge_sort(arr)
+  if arr.length == 0 
+  	return arr2
+  else
+  	#arr.each do |element|
+  	if arr.length > 1 
+  	  arr2 = arr[0..arr.length/2-1]
+  	  arr3 = arr[arr.length/2..-1]  	
+
+  	end
+  end  
+
+end
+
+
+
+
+
+
+#puts merge_sort([3,2,1])
+y = [1,2]
+#x = y.each_slice(2).to_a
+#puts x.inspect
+#puts 1.length
+
+puts y.shift
+puts y
---
+++ test.rb
@@ -0,0 +1,11 @@
+z = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+z.each do |item|
+#puts item
+end
+
+#puts z.length()
+#puts z.length/2-1
+puts z[0..z.length/2-1]
+puts "b"
+puts z[z.length/2..-1]
